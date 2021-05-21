0c198535656f3920ee0c369580021b4912dd45cb
Sherlock <tiennguyenhust@.com>
2021-05-22 00:22:43 +0200

    init project

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    printWidth: 80,
+    // tabWidth: 4,
+    useTabs: true,
+    semi: true,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: "always",
+    proseWrap: "never",
+    htmlWhitespaceSensitivity: "strict",
+    endOfLine: "lf",
+    [yaml] :{
+        useTabs: 2
+    }
+};
---
+++ contracts/TwitterFactory.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.13 <0.9.0;
+
+/**
+ @title TwitterFactory
+ @author Van Tien NGUYEN
+ @dev A contract dedicated to the creation of new twitter in the decentralised light-Twitter
+ */
+contract TwitterFactory {
+
+}
