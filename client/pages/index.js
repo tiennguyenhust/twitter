c3cb8021a7bca088692e922c31de4bcfec9828e8
Tri Tam HOANG <tritam.hoang@gmail.com>
2021-05-27 09:19:19 +0200

    Update front-end

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { eth, getInstance } from '../web3/provider' 
 import { Page, Center } from "../components/Layout"
 import TweetFactory from "../web3/artifacts/TweetFactory.json"
+// import Button from "../components/Button"
+import Modal from "../components/Modal"
 
 export default class IndexPage extends React.Component {
     async componentDidMount() {
@@ -74,8 +76,69 @@ export default class IndexPage extends React.Component {
         return (
             <Page>
                 <Center>
-                    <h1>hello world!</h1>
+                <h2>
+                    A <mark>decentralized</mark>, <mark>uncensorable</mark> Twitter clone built on Ethereum
+                </h2>
+
+                <div className="right-side">
+                    <div className="disclaimer">
+                    <p>
+                        MetaMask will automatically open and ask you to confirm a transaction.
+                    </p>
+                    <p>
+                        Please note that creating an account on the Ethereum blockchain costs a small amount of Ether.
+                    </p>
+                    </div>
+                </div>
                 </Center>
+
+                <style jsx global>{`
+                html, body {
+                    min-height: 100%;
+                }
+                body {
+                    background-color: #262740;
+                    background-image: url("/static/images/landing-bg.jpg");
+                    background-size: cover;
+                    background-position: center center;
+                }
+                `}</style>
+
+                <style jsx>{`
+                h2 {
+                    font-size: 50px;
+                    color: #FFFFFF;
+                    line-height: 78px;
+                    position: relative;
+                    text-transform: uppercase;
+                    max-width: 520px;
+                    display: inline-block;
+                }
+                mark {
+                    color: inherit;
+                    background-color: #9F99EC;
+                    padding: 0 7px;
+                }
+                .right-side {
+                    float: right;
+                    position: relative;
+                    max-width: 320px;
+                    text-align: center;
+                    margin-top: 120px;
+                }
+                .right-side :global(svg) {
+                    position: absolute;
+                    margin-left: -46px;
+                    margin-top: -8px;
+                }
+                .disclaimer {
+                    font-size: 14px;
+                    color: rgba(255,255,255,0.8);
+                    line-height: 23px;
+                    font-weight: 400;
+                    margin-top: 23px;
+                }
+                `}</style>
             </Page>
         )
     }
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Prepare:
+- install metamask on the browswer
+- setup metamask
+- install ganache-cli globally
+
+Steps to test front-end
+- ganache-cli
+- truffle migrate
+- npm run dev
+- open web browser at http://localhost:3000/
